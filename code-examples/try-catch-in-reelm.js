f89a585be1fa91b1f1757e0850d093b7e689427c
tihonove <tihonove@skbkontur.ru>
2016-06-16 21:25:40 +0500

    fix text

--- code-examples/try-catch-in-reelm.js
+++ code-examples/try-catch-in-reelm.js
@@ -1,15 +1,14 @@
 const reducer = defineReducer({})
-    .on('RefreshData', perform(function* () {
-        try {
-            const data =                                    /*1*/
-                yield call(async () => await api.getData());/*1*/
-
-            yield put({ type: 'DataRefreshed', data });
-        }
-        catch (e) {
-            yield put({
-                type: 'FailRefreshData',
-                message: e.toString(),
-            });
-        }
-    }));
+  .on('RefreshData', perform(function* () {
+    try {
+      const data =                                   /*1*/
+        yield call(async () => await api.getData()); /*1*/
+      yield put({ type: 'DataRefreshed', data });
+    }
+    catch (e) {
+      yield put({
+        type: 'FailRefreshData',
+        message: e.toString(),
+      });
+    }
+  }));
